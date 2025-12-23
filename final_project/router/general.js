93c7adf7c9acd94ef2cab93c840dfdbe61d35a2d
Vaishnavibitla4 <bitlavaishnavi4@gmail.com>
2025-12-23 13:49:17 -0500

    used async-await for book details based on ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,15 +27,29 @@ public_users.get('/', async function (req, res) {
 // ---------------------------------------------------
 // Get book details based on ISBN
 // ---------------------------------------------------
-public_users.get('/isbn/:isbn', function (req, res) {
+// Get book details based on ISBN using async/await
+public_users.get('/isbn/:isbn', async (req, res) => {
     const isbn = req.params.isbn;
 
-    if (books[isbn]) {
-        return res.status(200).json(books[isbn]);
-    } else {
-        return res.status(404).json({ message: "Book not found" });
+    try {
+        const getBookByISBN = (isbn) => {
+            return new Promise((resolve, reject) => {
+                if (books[isbn]) {
+                    resolve(books[isbn]);
+                } else {
+                    reject("Book not found");
+                }
+            });
+        };
+
+        const book = await getBookByISBN(isbn);
+        return res.status(200).json(book);
+
+    } catch (error) {
+        return res.status(404).json({ message: error });
     }
 });
+
 
 // ---------------------------------------------------
 // Get book details based on author
